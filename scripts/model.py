688efba335d987759b06688a7b6482a78e625929
Ephrem Mekonnen <ephrem8118@gmail.com>
2024-09-21 13:35:42 +0300

    logging is added

--- scripts/model.py
+++ scripts/model.py
@@ -2,4 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
+import os
+import logging
+
+# Define the log directory
+log_dir = r'c:\Users\ephre\Documents\Rossmann-Pharmaceuticals-Finance-1\Logs'
+
+# Create the log directory if it doesn't exist
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+# Configure logging
+logging.basicConfig(
+    filename=os.path.join(log_dir, 'model.log'),
+    filemode='w',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 
